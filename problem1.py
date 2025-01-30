2f87d4b7e38e9e47a3e92e065d8b7115217d4a30
Dewan <128119759+Dewan11@users.noreply.github.com>
2025-01-30 15:56:58 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,6 @@
+def upp ():
+  for i in range(65, 91):
+    print(chr(i))
+  for i in range(97, 123):
+    print(chr(i))
+upp()
---
+++ problem2.py
@@ -0,0 +1,13 @@
+def multi(num):
+    for i in range(1,11):
+      product = num*i
+      print(num, "*", i, "=", product)
+     
+number=int(input("enter the number :"))
+
+multi(number)
+        
+
+
+
+
